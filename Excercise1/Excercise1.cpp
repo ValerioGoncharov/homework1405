07db526b63a480a1bf5326fa5627ba62971bba76
ValerioGoncharov <i@valeriogoncharov.ru>
2025-05-14 16:03:41 +0300

    Initial commit

---
+++ Excercise1/Excercise1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+
+class Figure
+{
+private:
+	int sides;
+	std::string name;
+
+public:
+	int get_sides()
+	{
+		return sides;
+	}
+	std::string get_name()
+	{
+		return name;
+	}
+	Figure(int s, std::string n) : sides(s), name(n) {}
+};
+
+class Triangle : public Figure
+{
+public:
+	Triangle(int s, std::string n) : Figure(s, n) {}
+};
+
+class Rectangle : public Figure
+{
+public:
+	Rectangle(int s, std::string n) : Figure(s, n) {}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	Figure F1(0, "Фигура");
+	Triangle T1(3, "Треугольник");
+	Rectangle R1(4, "Четырёхугольник");
+
+	std::cout << "Количество сторон: " << std::endl;
+	std::cout << F1.get_name() << ": " << F1.get_sides() << std::endl;
+	std::cout << T1.get_name() << ": " << T1.get_sides() << std::endl;
+	std::cout << R1.get_name() << ": " << R1.get_sides() << std::endl;
+
+	return 0;
+}
